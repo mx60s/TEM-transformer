cafdbd11a2f9fe6d45d1125514b6d6d4623a2d32
Maggie von Ebers <vonebersm@gmail.com>
2023-07-24 22:28:32 -0500

    clean up walks and format and add very basic transformer classes

--- model.py
+++ model.py
@@ -8,27 +8,37 @@ I'm not sure if I'll actually finish this
 
 @author: mx60s
 """
+import copy
+import math
+import os
+import pdb
+from tempfile import TemporaryDirectory
+from typing import Tuple
+
 # Standard modules
 import numpy as np
 import torch
-import pdb
-import copy
 from scipy.stats import truncnorm
+from torch import Tensor, nn
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import dataset
+
 # Custom modules
 import utils
 
+
 class Model(torch.nn.Module):
     def __init__(self, params):
-        super(Model, self).__init__()
+        super().__init__()
         self.hyper = copy.deepcopy(params)
         self.init_trainable()
-    
+
     def forward(self, x):
         raise NotImplementedError
 
     def iteration(self, x):
         raise NotImplementedError
-        
+
     def inference(self, x):
         raise NotImplementedError
 
@@ -41,8 +51,223 @@ class Model(torch.nn.Module):
     def init_trainable(self):
         raise NotImplementedError
 
+
+# Might need to rename since these are essentially g
+class PositionalEncoding(nn.Module):
+    def __init__(self, params):
+        super().__init__()
+        dropout = params["dropout"]
+        max_len = params["max_len"]
+        d_model = params["d_model"]
+
+        self.dropout = nn.Dropout(p=dropout)
+        self.pe = nn.Linear(
+            max_len, d_model
+        )  # dim was (max_len, 1, d_model) in original, may need to reshape
+        # I think this also needs nonlinearity but it doesn't say what in the paper
+        # look for other examples in the original TEM code of these learnable matrices
+        # g_t+1 = f(g_t * W_a) where W_a is learnable action-dependent matrix
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x += self.pe
+        return self.dropout(x)  # do we still want a dropout?
+
+
+class TransformerEmbedding(nn.Module):
+    def __init__(self):
+        # combine a positional encoding w an embedding
+        # paper mentions some embedding but not sure
+        raise NotImplementedError
+
+    def forward(self, x):
+        raise NotImplementedError
+
+
+class ScaleDotProductAttention(nn.Module):
+    """
+    compute scale dot product attention
+
+    Query : given sentence that we focused on (decoder)
+    Key : every sentence to check relationship with Qeury(encoder)
+    Value : every sentence same with Key (encoder)
+    """
+
+    def __init__(self):
+        super(ScaleDotProductAttention, self).__init__()
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, q, k, v, mask=None, e=1e-12):
+        # input is 4 dimension tensor
+        # [batch_size, head, length, d_tensor]
+        batch_size, head, length, d_tensor = k.size()
+
+        # 1. dot product Query with Key^T to compute similarity
+        k_t = k.transpose(2, 3)  # transpose
+        score = (q @ k_t) / math.sqrt(d_tensor)  # scaled dot product
+
+        # 2. apply masking (opt)
+        if mask is not None:
+            score = score.masked_fill(mask == 0, -10000)
+
+        # 3. pass them softmax to make [0, 1] range
+        score = self.softmax(score)
+
+        # 4. multiply with Value
+        v = score @ v
+
+        return v, score
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, n_head):
+        super(MultiHeadAttention, self).__init__()
+        self.n_head = n_head
+        self.attention = ScaleDotProductAttention()
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_concat = nn.Linear(d_model, d_model)
+
+    def forward(self, q, k, v, mask=None):
+        # 1. dot product with weight matrices
+        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
+
+        # 2. split tensor by number of heads
+        q, k, v = self.split(q), self.split(k), self.split(v)
+
+        # 3. do scale dot product to compute similarity
+        out, attention = self.attention(q, k, v, mask=mask)
+
+        # 4. concat and pass to linear layer
+        out = self.concat(out)
+        out = self.w_concat(out)
+
+        # 5. visualize attention map
+        # TODO : we should implement visualization
+
+        return out
+
+    def split(self, tensor):
+        """
+        split tensor by number of head
+
+        :param tensor: [batch_size, length, d_model]
+        :return: [batch_size, head, length, d_tensor]
+        """
+        batch_size, length, d_model = tensor.size()
+
+        d_tensor = d_model // self.n_head
+        tensor = tensor.view(batch_size, length, self.n_head, d_tensor).transpose(1, 2)
+        # it is similar with group convolution (split by number of heads)
+
+        return tensor
+
+    def concat(self, tensor):
+        """
+        inverse function of self.split(tensor : torch.Tensor)
+
+        :param tensor: [batch_size, head, length, d_tensor]
+        :return: [batch_size, length, d_model]
+        """
+        batch_size, head, length, d_tensor = tensor.size()
+        d_model = head * d_tensor
+
+        tensor = tensor.transpose(1, 2).contiguous().view(batch_size, length, d_model)
+        return tensor
+
+
+# layernorm on the positional encodings (not in the RNN, but on the input to transformer)
+# we use fixed weights on the layer norm, i.e. is is just a z score of g
+
+
+# TODO explore using learnable layer norm
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, ffn_hidden, n_head, drop_prob):
+        super(EncoderLayer, self).__init__()
+        self.attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+        self.norm1 = LayerNorm(d_model=d_model)  # TODO replace w z-score
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
+        # TODO this is dumb replace
+        # self.ffn = PositionwiseFeedForward(d_model=d_model, hidden=ffn_hidden, drop_prob=drop_prob)
+        self.norm2 = LayerNorm(d_model=d_model)  # TODO replace w z-score
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+    def forward(self, x, src_mask):
+        # 1. compute self attention
+        _x = x
+        x = self.attention(q=x, k=x, v=x, mask=src_mask)
+
+        # 2. add and norm
+        x = self.dropout1(x)
+        x = self.norm1(x + _x)
+
+        # 3. positionwise feed forward network
+        _x = x
+        x = self.ffn(x)
+
+        # 4. add and norm
+        x = self.dropout2(x)
+        x = self.norm2(x + _x)
+        return x
+
+
+class Encoder(nn.Module):
+    def __init__(
+        self,
+        enc_voc_size,
+        max_len,
+        d_model,
+        ffn_hidden,
+        n_head,
+        n_layers,
+        drop_prob,
+        device,
+    ):
+        super().__init__()
+        self.emb = TransformerEmbedding()
+
+        self.layers = nn.ModuleList(
+            [
+                EncoderLayer(
+                    d_model=d_model,
+                    ffn_hidden=ffn_hidden,
+                    n_head=n_head,
+                    drop_prob=drop_prob,
+                )
+                for _ in range(n_layers)
+            ]
+        )
+
+    def forward(self, x, src_mask):
+        x = self.emb(x)
+
+        for layer in self.layers:
+            x = layer(x, src_mask)
+
+        return x
+
+
 class Iteration:
-    def __init__(self, g = None, x = None, a = None, L = None, M = None, g_gen = None, p_gen = None, x_gen = None, x_logits = None, x_inf = None, g_inf = None, p_inf = None):
+    def __init__(
+        self,
+        g=None,
+        x=None,
+        a=None,
+        L=None,
+        M=None,
+        g_gen=None,
+        p_gen=None,
+        x_gen=None,
+        x_logits=None,
+        x_inf=None,
+        g_inf=None,
+        p_inf=None,
+    ):
         # Copy all inputs
         self.g = g
         self.x = x
@@ -62,9 +287,12 @@ class Iteration:
         observation = self.x.detach().numpy()
         predictions = [tensor.detach().numpy() for tensor in self.x_gen]
         # Did the model predict the right observation in this iteration?
-        accuracy = [np.argmax(prediction, axis=-1) == np.argmax(observation, axis=-1) for prediction in predictions]
+        accuracy = [
+            np.argmax(prediction, axis=-1) == np.argmax(observation, axis=-1)
+            for prediction in predictions
+        ]
         return accuracy
-    
+
     def detach(self):
         # Detach all tensors contained in this iteration
         self.L = [tensor.detach() for tensor in self.L]
@@ -77,4 +305,3 @@ class Iteration:
         self.p_inf = [tensor.detach() for tensor in self.p_inf]
         # Return self after detaching everything
         return self
-        
--- test.py
+++ test.py
@@ -12,6 +12,7 @@ import torch
 import glob
 import matplotlib.pyplot as plt
 import importlib.util
+
 # Own module imports. Note how model module is not imported, since we'll used the model from the training run
 import world
 import analyse
@@ -22,42 +23,57 @@ np.random.seed(0)
 torch.manual_seed(0)
 
 # Choose which trained model to load
-date = '2020-10-19' # 2020-10-13 run 0 for successful node agent
-run = '0'
-index = '32000'
+date = "2020-10-19"  # 2020-10-13 run 0 for successful node agent
+run = "0"
+index = "32000"
 
 # Load the model: use import library to import module from specified path
-model_spec = importlib.util.spec_from_file_location("model", '../Summaries/' + date + '/run' + run + '/script/model.py')
+model_spec = importlib.util.spec_from_file_location(
+    "model", "../Summaries/" + date + "/run" + run + "/script/model.py"
+)
 model = importlib.util.module_from_spec(model_spec)
 model_spec.loader.exec_module(model)
 
 # Load the parameters of the model
-params = torch.load('../Summaries/' + date + '/run' + run + '/model/params_' + index + '.pt')
+params = torch.load(
+    "../Summaries/" + date + "/run" + run + "/model/params_" + index + ".pt"
+)
 # Create a new tem model with the loaded parameters
 tem = model.Model(params)
 # Load the model weights after training
-model_weights = torch.load('../Summaries/' + date + '/run' + run + '/model/tem_' + index + '.pt')
+model_weights = torch.load(
+    "../Summaries/" + date + "/run" + run + "/model/tem_" + index + ".pt"
+)
 # Set the model weights to the loaded trained model weights
 tem.load_state_dict(model_weights)
 # Make sure model is in evaluate mode (not crucial because it doesn't currently use dropout or batchnorm layers)
 tem.eval()
 
 # Make list of all the environments that this model was trained on
-envs = list(glob.iglob('../Summaries/' + date + '/run' + run + '/script/envs/*'))
+envs = list(glob.iglob("../Summaries/" + date + "/run" + run + "/script/envs/*"))
 # Set which environments will include shiny objects
 shiny_envs = [False, False, True, True]
 # Set the number of walks to execute in parallel (batch size)
 n_walks = len(shiny_envs)
 # Select environments from the environments included in training
-environments = [world.World(graph, randomise_observations=True, shiny=(params['shiny'] if shiny_envs[env_i] else None)) 
-                for env_i, graph in enumerate(np.random.choice(envs, n_walks))]
+environments = [
+    world.World(
+        graph,
+        randomise_observations=True,
+        shiny=(params["shiny"] if shiny_envs[env_i] else None),
+    )
+    for env_i, graph in enumerate(np.random.choice(envs, n_walks))
+]
 # Determine the length of each walk
 walk_len = np.median([env.n_locations * 50 for env in environments]).astype(int)
 # And generate walks for each environment
 walks = [env.generate_walks(walk_len, 1)[0] for env in environments]
 
 # Generate model input from specified walk and environment: group steps from all environments together to feed to model in parallel
-model_input = [[[[walks[i][j][k]][0] for i in range(len(walks))] for k in range(3)] for j in range(walk_len)]
+model_input = [
+    [[[walks[i][j][k]][0] for i in range(len(walks))] for k in range(3)]
+    for j in range(walk_len)
+]
 for i_step, step in enumerate(model_input):
     model_input[i_step][1] = torch.stack(step[1], dim=0)
 
@@ -69,10 +85,14 @@ with torch.no_grad():
 include_stay_still = True
 
 # Compare trained model performance to a node agent and an edge agent
-correct_model, correct_node, correct_edge = analyse.compare_to_agents(forward, tem, environments, include_stay_still=include_stay_still)
+correct_model, correct_node, correct_edge = analyse.compare_to_agents(
+    forward, tem, environments, include_stay_still=include_stay_still
+)
 
 # Analyse occurrences of zero-shot inference: predict the right observation arriving from a visited node with a new action
-zero_shot = analyse.zero_shot(forward, tem, environments, include_stay_still=include_stay_still)
+zero_shot = analyse.zero_shot(
+    forward, tem, environments, include_stay_still=include_stay_still
+)
 
 # Generate occupancy maps: how much time TEM spends at every location
 occupation = analyse.location_occupation(forward, tem, environments)
@@ -86,33 +106,99 @@ from_acc, to_acc = analyse.location_accuracy(forward, tem, environments)
 # Choose which environment to plot
 env_to_plot = 0
 # And when averaging environments, e.g. for calculating average accuracy, decide which environments to include
-envs_to_avg = shiny_envs if shiny_envs[env_to_plot] else [not shiny_env for shiny_env in shiny_envs]
+envs_to_avg = (
+    shiny_envs
+    if shiny_envs[env_to_plot]
+    else [not shiny_env for shiny_env in shiny_envs]
+)
 
 # Plot results of agent comparison and zero-shot inference analysis
 filt_size = 41
 plt.figure()
-plt.plot(analyse.smooth(np.mean(np.array([env for env_i, env in enumerate(correct_model) if envs_to_avg[env_i]]),0)[1:], filt_size), label='tem')
-plt.plot(analyse.smooth(np.mean(np.array([env for env_i, env in enumerate(correct_node) if envs_to_avg[env_i]]),0)[1:], filt_size), label='node')
-plt.plot(analyse.smooth(np.mean(np.array([env for env_i, env in enumerate(correct_edge) if envs_to_avg[env_i]]),0)[1:], filt_size), label='edge')
+plt.plot(
+    analyse.smooth(
+        np.mean(
+            np.array(
+                [env for env_i, env in enumerate(correct_model) if envs_to_avg[env_i]]
+            ),
+            0,
+        )[1:],
+        filt_size,
+    ),
+    label="tem",
+)
+plt.plot(
+    analyse.smooth(
+        np.mean(
+            np.array(
+                [env for env_i, env in enumerate(correct_node) if envs_to_avg[env_i]]
+            ),
+            0,
+        )[1:],
+        filt_size,
+    ),
+    label="node",
+)
+plt.plot(
+    analyse.smooth(
+        np.mean(
+            np.array(
+                [env for env_i, env in enumerate(correct_edge) if envs_to_avg[env_i]]
+            ),
+            0,
+        )[1:],
+        filt_size,
+    ),
+    label="edge",
+)
 plt.ylim(0, 1)
 plt.legend()
-plt.title('Zero-shot inference: ' + str(np.mean([np.mean(env) for env_i, env in enumerate(zero_shot) if envs_to_avg[env_i]]) * 100) + '%')
+plt.title(
+    "Zero-shot inference: "
+    + str(
+        np.mean(
+            [np.mean(env) for env_i, env in enumerate(zero_shot) if envs_to_avg[env_i]]
+        )
+        * 100
+    )
+    + "%"
+)
 plt.show()
 
 # Plot rate maps for all cells
-plot.plot_cells(p[env_to_plot], g[env_to_plot], environments[env_to_plot], n_f_ovc=(params['n_f_ovc'] if 'n_f_ovc' in params else 0), columns = 25)
+plot.plot_cells(
+    p[env_to_plot],
+    g[env_to_plot],
+    environments[env_to_plot],
+    n_f_ovc=(params["n_f_ovc"] if "n_f_ovc" in params else 0),
+    columns=25,
+)
 
 # Plot accuracy separated by location
 plt.figure()
-ax = plt.subplot(1,2,1)
+ax = plt.subplot(1, 2, 1)
 plot.plot_map(environments[env_to_plot], np.array(to_acc[env_to_plot]), ax)
-ax.set_title('Accuracy to location')
-ax = plt.subplot(1,2,2)
+ax.set_title("Accuracy to location")
+ax = plt.subplot(1, 2, 2)
 plot.plot_map(environments[env_to_plot], np.array(from_acc[env_to_plot]), ax)
-ax.set_title('Accuracy from location')
+ax.set_title("Accuracy from location")
 
 # Plot occupation per location, then add walks on top
-ax = plot.plot_map(environments[env_to_plot], np.array(occupation[env_to_plot])/sum(occupation[env_to_plot])*environments[env_to_plot].n_locations, 
-                   min_val=0, max_val=2, ax=None, shape='square', radius=1/np.sqrt(environments[env_to_plot].n_locations))
-ax = plot.plot_walk(environments[env_to_plot], walks[env_to_plot], ax=ax, n_steps=max(1, int(len(walks[env_to_plot])/500)))
-plt.title('Walk and average occupation')
+ax = plot.plot_map(
+    environments[env_to_plot],
+    np.array(occupation[env_to_plot])
+    / sum(occupation[env_to_plot])
+    * environments[env_to_plot].n_locations,
+    min_val=0,
+    max_val=2,
+    ax=None,
+    shape="square",
+    radius=1 / np.sqrt(environments[env_to_plot].n_locations),
+)
+ax = plot.plot_walk(
+    environments[env_to_plot],
+    walks[env_to_plot],
+    ax=ax,
+    n_steps=max(1, int(len(walks[env_to_plot]) / 500)),
+)
+plt.title("Walk and average occupation")
